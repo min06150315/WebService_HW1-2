465102ba202364719e823de5a80569ca2fc0f6b4
min06150315 <min06150315@gmail.com>
2024-10-29 13:59:53 +0900

    Add Champion.java

---
+++ src/main/java/org/example/Champion.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Champion {
+    private int id;
+    private String name;
+    private String position;
+    private String damageType;
+    private String passive;
+    private String Q;
+    private String W;
+    private String E;
+    private String R;
+}
