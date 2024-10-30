d735f51d8e25cf91feb5250cea05cb7e0ee7466c
min06150315 <min06150315@gmail.com>
2024-10-31 02:54:40 +0900

    Update Champion.java (Champion 객체에 region(챔피언의 소속) 변수 추가)

--- src/main/java/org/example/Champion.java
+++ src/main/java/org/example/Champion.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 public class Champion {
     private int id; // 고유 id
     private String name; // 챔피언 이름
+    private String region; // 챔피언의 소속 (ex. 데미시아, 프렐요드, 필트오버, 아이오니아...)
     private String position; // 챔피언 역할 (암살자, 전사, 마법사, 원거리 딜러, 서포터, 탱커)
     private String damageType; // 데미지 유형 (물리, 마법, 혼합)
     private String skillPassive; // 패시브 스킬 이름
@@ -18,9 +19,10 @@ public class Champion {
     private String skillR; // R 스킬 이름
     private String createDate; // 데이터가 생성된 날짜
 
-    public Champion(int id, String name, String position, String damageType, String skillPassive, String skillQ, String skillW, String skillE, String skillR, String createDate) {
+    public Champion(int id, String name, String region, String position, String damageType, String skillPassive, String skillQ, String skillW, String skillE, String skillR, String createDate) {
         this.id = id;
         this.name = name;
+        this.region = region;
         this.position = position;
         this.damageType = damageType;
         this.skillPassive = skillPassive;
@@ -33,6 +35,6 @@ public class Champion {
 
     @Override
     public String toString() {
-        return String.format("%d %s\t(%s %s)\t[%s\t%s\t%s\t%s\t%s]  -  %s", id, name, position, damageType, skillPassive, skillQ, skillW, skillE, skillR, createDate);
+        return String.format("%d. %s\t(%s  %s  %s)\t[%s\t%s\t%s\t%s\t%s]  -  %s", id, name, region, position, damageType, skillPassive, skillQ, skillW, skillE, skillR, createDate);
     }
 }
